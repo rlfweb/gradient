93a2402c276a1f062a14e26ff473c40e9e4888f0
Rebecca French <rlfweb8@gmail.com>
2019-12-19 20:53:19 +0000

    React from lesson with James

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Header from './Header';
+import Nav from './Nav'; 
+
+class App extends React.Component {
+  render() {
+    return  (
+      <div>
+{/* // You add every file name within here */}
+        <Header />
+
     </div>
-  );
+    );
+  }
 }
+
+//in React you have to use className not class, as class is a JS reserved word. 
+
+
+// DELETE ALL THIS STUFF AND REPLACE WITH THE ABOVE
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  render() {
+    return  (
+    <div>
+            <h1> This is my title    </h1>
+            <h2> This is my subtitle    </h2>
+    </div>
+    );
+  }
+}
+
+// REMEMBER TO CHANGE THIS BELOW TO FILENAME i.e. from App to Header
+export default Header;
