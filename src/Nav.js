8d73259120d8a941a642d2fd0dad39a25627a54d
Rebecca French <rlfweb8@gmail.com>
2020-01-03 14:03:09 +0000

    added the Nav

--- src/Nav.js
+++ src/Nav.js
@@ -3,13 +3,32 @@ import React from 'react';
 class Nav extends React.Component {
 
   render() {
-    return  (
-    <div>
-            <h1> This is the Nav     </h1>
-    </div>
-    );
-  }
-}
+    return (
+      
+       
+       
+       <div class="container">
 
-// REMEMBER TO CHANGE THIS BELOW TO FILENAME i.e. from App to Header
-export default Nav;
+<div class="row headColour">
+    
+    <div class="col-12 col-md-4 logo">
+        GRADIENT
+    </div>
+     
+    <div class="date col-12 col-md-4 text-lg-center">
+        Saturday 14 December 2019
+    </div>
+
+    <div class="signInAndGreeting col-12 col-md-4 text-lg-right">
+        Log in / Sign up / Hello [Susan]
+    </div>
+</div>
+
+
+        </div>
+        );
+      }
+    }
+    
+    // REMEMBER TO CHANGE THIS BELOW TO FILENAME i.e. from App to Header
+    export default Nav;
