4a534ea907a26b3f11e03d2e309563c0aa2d282a
Rebecca French <rlfweb8@gmail.com>
2020-03-07 11:39:47 +0000

    Functioning well. Tidying up app.js

--- src/App.js
+++ src/App.js
@@ -11,26 +11,35 @@ import HomeTaskCount from "./components/HomeTaskCount";
 import Footer from "./components/Footer";
 import axios from "axios";
 
-//Filter the data from database:
-//const workTaskData = this.state;
-//const workTasks = workTaskData.filter( (tasks) => tasks.category.includes("WORK"));
+// STILL TO DO IF TIME:
+// Get 'Name' from users database
+// Get Search function working
+// The app is 'bloated' with duplicated code and could be consolidated. James says 'add WORK and HOME in as parameters and then don't have to have duplicate code'
+// Clear previous taskDescription from input field after 'add task' is clicked
+
+// NOTES
+// Tasks need a unique id, so if there are more than one identical task, both don't get deleted, when one delete button is clicked (uuid is in backend)
+// As state lives in app.js it is only within app.js that we should be altering the state, so get, post, put, delete go in here
+// className instead of class. class is a JS reserved word
 
 class App extends React.Component {
-  // good to give tasks a unique id, so they are unique, so if there are more than one identical task, both don't get deleted, when one delete button is clicked
   state = {
     workTasks: [],
     homeTasks: []
   };
 
-  // this fires as soon as page loads, which says bring my content in from my database
+  // this fires as soon as page loads, which says "bring my content in from my database"
   componentDidMount = () => {
     // Fetch tasks from API
     axios
       .get("https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks")
       .then(response => {
-
-        const workTasks = response.data.tasks.filter( (task) => task.category.includes("WORK"));
-        const homeTasks = response.data.tasks.filter( (task) => task.category.includes("HOME"));
+        const workTasks = response.data.tasks.filter(task =>
+          task.category.includes("WORK")
+        );
+        const homeTasks = response.data.tasks.filter(task =>
+          task.category.includes("HOME")
+        );
         // handle success
         this.setState({
           workTasks: workTasks,
@@ -42,14 +51,6 @@ class App extends React.Component {
         console.error(error);
       });
   };
-
-  //POST IS GOING TO BE A CLICK EVENT
-  // this website has useful info for us https://www.npmjs.com/package/axios
-  //axios.post
-  // axios.delete
-  // axios.put
-
-  // As state lives in app.js it is only within app.js that we should be altering the state, so the delete stuff goes in here
 
   deleteWorkTask = workTaskId => {
     // Tasks will be deleted when this function runs
@@ -77,87 +78,46 @@ class App extends React.Component {
       });
   };
 
-  // As state lives in app.js it is only within app.js that we should be altering the state, so the edit/complete stuff goes in here
-
-
-
-
-
-
   completeWorkTask = workTaskId => {
-		
-  
     const workTaskToEdit = {
-      taskId : workTaskId
-    }
+      taskId: workTaskId
+    };
 
-
-		axios
-			.put(
-				`https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks/${workTaskId}`, workTaskToEdit,
-			)
-			.then(response => {
-
+    axios
+      .put(
+        `https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks/${workTaskId}`,
+        workTaskToEdit
+      )
+      .then(response => {
         // Firstly find the task that needs to be updated
-        const workTasksBeingUpdated = this.state.workTasks; 
+        const workTasksBeingUpdated = this.state.workTasks;
         for (let i = 0; i < workTasksBeingUpdated.length; i++) {
           const workTask = workTasksBeingUpdated[i];
-          
+
           if (workTask.taskId === workTaskId) {
             workTask.completed = true;
             break;
           }
         }
-          
-				this.setState({
-					workTasks: workTasksBeingUpdated
-				});
-			})
-			.catch(function(error) {
-				console.error(error);
-			});
-	};
 
-  // completeWorkTask = workTaskId => {
-  //   // Firstly find the task that needs to be updated
-  //   const workTasksBeingUpdated = this.state.workTasks; // this is an array of tasks
-  //   for (let i = 0; i < workTasksBeingUpdated.length; i++) {
-  //     const workTask = workTasksBeingUpdated[i];
-
-  //     if (workTask.taskId === workTaskId) {
-  //       // we need to update a property on the identified task
-  //       workTask.completed = true;
-  //       break;
-  //     }
-  //   }
-  //   // just need to loop through the array until we find the one that matches and then boot us out of the array
-
-  //   // Upate state to reflect the changes made to the task
-  //   this.setState({
-  //     workTasks: workTasksBeingUpdated
-  //   });
-  // };
-
-
-
-
-
-
-  
-
-
-
+        this.setState({
+          workTasks: workTasksBeingUpdated
+        });
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
 
   addWorkTask = workTaskDescription => {
-    // Define the task that is being added
-
+    // Define the task that is being added i.e. a json object with these data types inside
     const workTaskToAdd = {
       taskDescription: workTaskDescription,
       completed: 0,
       category: "WORK",
       userId: 1
     };
-// add WORK in as a parameter and then don't have to have duplicate code. 
+
     axios
       .post(
         "https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks",
@@ -171,7 +131,6 @@ class App extends React.Component {
 
         // add the 'taskToAdd' to the array of tasks in state
         currentWorkTasks.push(workTaskToAdd);
-    
 
         // update the state
         this.setState({
@@ -184,12 +143,7 @@ class App extends React.Component {
       });
   };
 
-  // As state lives in app.js it is only within app.js that we should be altering the state, so the delete stuff goes in here
-
   deleteHomeTask = homeTaskId => {
-    // Tasks will be deleted when this function runs
-
-    // I need the list of tasks from state
     const homeTasks = this.state.homeTasks;
 
     axios
@@ -197,74 +151,52 @@ class App extends React.Component {
         `https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks/${homeTaskId}`
       )
       .then(response => {
-    // Make sure the id of what we are deleting matches the id of what we want to delete
-    const updatedHomeTasks = homeTasks.filter(
-      item => item.taskId !== homeTaskId
-    );
+        const updatedHomeTasks = homeTasks.filter(
+          item => item.taskId !== homeTaskId
+        );
 
-    // I need to update the state with the new array of tasks i.e. without the one that has just been deleted
-    this.setState({
-      homeTasks: updatedHomeTasks
-    });
-  })
-  .catch(error => {
-    console.error(error);
-  });
-};
+        this.setState({
+          homeTasks: updatedHomeTasks
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-  // As state lives in app.js it is only within app.js that we should be altering the state, so the edit/complete stuff goes in here
-
-
-
-
-
-
-
-
-  
   completeHomeTask = homeTaskId => {
-      
     const homeTaskToEdit = {
-      taskId : homeTaskId
-    }
+      taskId: homeTaskId
+    };
 
     axios
-			.put(
-				`https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks/${homeTaskId}`, homeTaskToEdit,
-			)
-			.then(response => {
-// Firstly find the task that needs to be updated
-    const homeTasksBeingUpdated = this.state.homeTasks; // this is an array of tasks
-    for (let i = 0; i < homeTasksBeingUpdated.length; i++) {
-      const homeTask = homeTasksBeingUpdated[i];
+      .put(
+        `https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks/${homeTaskId}`,
+        homeTaskToEdit
+      )
+      .then(response => {
+        const homeTasksBeingUpdated = this.state.homeTasks;
+        for (let i = 0; i < homeTasksBeingUpdated.length; i++) {
+          const homeTask = homeTasksBeingUpdated[i];
 
-      if (homeTask.taskId === homeTaskId) {
-        // we need to update a property on the identified task
-        homeTask.completed = true;
-        break;
-      }
-    }
-    // just need to loop through the array until we find the one that matches and then boot us out of the array
+          if (homeTask.taskId === homeTaskId) {
+            homeTask.completed = true;
+            break;
+          }
+        }
 
-    // Upate state to reflect the changes made to the task
-    this.setState({
-      homeTasks: homeTasksBeingUpdated
-    });
-  })
-  .catch(function(error) {
-    console.error(error);
-  });
-};
-
-
-
-
-
-
+        this.setState({
+          homeTasks: homeTasksBeingUpdated
+        });
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+  };
 
   // just after taskDescription is where we might also set priorities or dates for tasks to be done by
+
   addHomeTask = homeTaskDescription => {
-    // Define the task that is being added
     const homeTaskToAdd = {
       taskDescription: homeTaskDescription,
       completed: 0,
@@ -273,30 +205,24 @@ class App extends React.Component {
     };
 
     axios
-    .post(
-      "https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks",
-      homeTaskToAdd
-    )
-    .then(response => {
-      homeTaskToAdd.taskId = response.data.tasks.taskId;
-      console.log(homeTaskToAdd);
-    // get the current list of tasks from state
-    const currentHomeTasks = this.state.homeTasks;
-    // add the 'taskToAdd' to the array of tasks in state
-    currentHomeTasks.push(homeTaskToAdd);
-    // update the state
-    this.setState({
-      homeTasks: currentHomeTasks
-    });
-  })
-  .catch(error => {
-    // handle error
-    console.error(error);
-  });
-};
+      .post(
+        "https://fvnx69glt6.execute-api.eu-west-2.amazonaws.com/dev/tasks",
+        homeTaskToAdd
+      )
+      .then(response => {
+        homeTaskToAdd.taskId = response.data.tasks.taskId;
+        console.log(homeTaskToAdd);
+        const currentHomeTasks = this.state.homeTasks;
+        currentHomeTasks.push(homeTaskToAdd);
+        this.setState({
+          homeTasks: currentHomeTasks
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-
-  
   render() {
     return (
       <div>
@@ -307,7 +233,7 @@ class App extends React.Component {
             <div className="col-12 col-md-6">
               <WorkTaskCount workTaskCount={this.state.workTasks.length} />
               <WorkTaskList
-                workTaskCollection={this.state.workTasks} 
+                workTaskCollection={this.state.workTasks}
                 deleteWorkTaskFunc={this.deleteWorkTask}
                 completedWorkTaskFunc={this.completeWorkTask}
               />
@@ -317,7 +243,7 @@ class App extends React.Component {
             <div className="col-12 col-md-6">
               <HomeTaskCount homeTaskCount={this.state.homeTasks.length} />
               <HomeTaskList
-                homeTaskCollection={this.state.homeTasks} 
+                homeTaskCollection={this.state.homeTasks}
                 deleteHomeTaskFunc={this.deleteHomeTask}
                 completedHomeTaskFunc={this.completeHomeTask}
               />
@@ -332,8 +258,6 @@ class App extends React.Component {
 }
 
 export default App;
-
-//in React you have to use className not class, as class is a JS reserved word.
 
 // DELETE ALL THIS AND REPLACE WITH THE ABOVE
 // function App() {
