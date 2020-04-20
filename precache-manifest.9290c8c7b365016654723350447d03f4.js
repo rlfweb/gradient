10feeabeb96029e28dc6aff3bbbef8680f6a81a4
Rebecca French <rlfweb8@gmail.com>
2020-04-20 14:00:08 +0100

    Updates

---
+++ precache-manifest.9290c8c7b365016654723350447d03f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add71836ae37b98a85ab96e328f29a7c",
+    "url": "/gradient/index.html"
+  },
+  {
+    "revision": "a1281c42a3c6db016f5e",
+    "url": "/gradient/static/css/main.dc46a547.chunk.css"
+  },
+  {
+    "revision": "b5dcd33c226fc74dfcb7",
+    "url": "/gradient/static/js/2.377723a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gradient/static/js/2.377723a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1281c42a3c6db016f5e",
+    "url": "/gradient/static/js/main.b3ee3427.chunk.js"
+  },
+  {
+    "revision": "8bf6f407448d0f93788c",
+    "url": "/gradient/static/js/runtime-main.8e3b4907.js"
+  }
+]);
